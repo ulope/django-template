4c9e6403bd353e1f61a3d0ffa9dde0736cb1a4be
Ulrich Petri <ulo@ulo.pe>
2013-04-23 16:51:37 +0200

    nicified the setup script a little

--- mkproject.sh
+++ mkproject.sh
@@ -3,20 +3,31 @@
 set -e
 
 : ${VIRTUAL_ENV:?"This script needs to be run inside of an activated virtualenv"}
+PROJECT_NAME=${1:-$(basename $VIRTUAL_ENV)}
 
-PROJECT_NAME=$(basename $VIRTUAL_ENV)
+echo "Bootstrapping project '$PROJECT_NAME':"
 
-pip install "django<1.6"
+echo -n   " - Installing Django..."
+pip install "Django<1.6" > /dev/null
+echo -e "\r - Successfully installed Django."
 
+echo -n   " - Setting up Django project..."
 django-admin.py startproject --template=https://github.com/ulope/django-template/archive/master.zip --extension=py,key $PROJECT_NAME
+echo -e "\r - Done setting up Django project"
 
 cd $PROJECT_NAME
 
-pip install -r reqs/default.txt
-pip install -r reqs/dev.txt
+git init > /dev/null
+
+echo -n   " - Installing dependencies..."
+pip install -r reqs/default.txt > /dev/null
+pip install -r reqs/dev.txt > /dev/null
+echo -e "\r - Successfully installed dependencies."
 
 chmod +x manage.py
 
 find . -type f -name ".keep" | xargs rm
 rm mkproject.sh
 rm README
+
+echo -e "Done.\nYour project is located in $(pwd)"
